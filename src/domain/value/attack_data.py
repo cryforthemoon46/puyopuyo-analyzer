209c052e83399e3084b362b364b1d012990b22d5
cryforthemoon46 <cryforthemoon46@gmail.com>
2021-04-14 17:34:06 +0900

    生成時にplayer_numの引数を不要にする

--- src/domain/value/attack_data.py
+++ src/domain/value/attack_data.py
@@ -1,8 +1,8 @@
 class AttackData:
-    def __init__(self, player_num: int):
+    def __init__(self):
         self.is_valid = False
         self.frame_num = None
-        self.player_num = player_num
+        self.player_num = None
         self.chain_num = 0
         self.is_main_chain = False
         self.eliminated_num = 0
